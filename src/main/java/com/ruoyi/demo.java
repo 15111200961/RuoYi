3eaef58ee5bd23064029fb485cdc67cc557ef579
15111200961 <15111200961@163.com>
2019-09-06 16:40:38 +0800

    写了

--- src/main/java/com/ruoyi/demo.java
+++ src/main/java/com/ruoyi/demo.java
@@ -3,8 +3,8 @@ package com.ruoyi;
 public class demo {
     public static void main(String[] args) {
         int i =1;
-        int b =2;
         System.out.println(i);
-        System.out.println(b);
+        System.out.println(123);
+        System.out.println(222);
     }
 }
