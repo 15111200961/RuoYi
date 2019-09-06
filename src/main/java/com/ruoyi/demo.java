d5781e78f7220ee4584b15d1b47d8e762082284d
15111200961 <15111200961@163.com>
2019-09-06 16:38:05 +0800

    修改

--- src/main/java/com/ruoyi/demo.java
+++ src/main/java/com/ruoyi/demo.java
@@ -3,7 +3,7 @@ package com.ruoyi;
 public class demo {
     public static void main(String[] args) {
         int i =1;
-        int b =0;
+        int b =2;
         System.out.println(i);
         System.out.println(b);
     }
