03f80bd6485b47f37dd146e7456adad4ba76457c
15111200961 <15111200961@163.com>
2019-09-15 16:41:53 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/ruoyi/demo.java

--- src/main/java/com/ruoyi/demo.java
+++ src/main/java/com/ruoyi/demo.java
@@ -1,8 +1,11 @@
 package com.ruoyi;
 
+import org.junit.Test;
+
 import java.math.BigDecimal;
 
 public class demo {
+
     public static void main(String[] args) {
         BigDecimal a = new BigDecimal("123.00");
         BigDecimal b = new BigDecimal("233.00");
@@ -11,5 +14,12 @@ public class demo {
         sb.append("23");
         System.out.println(sb);
         System.out.println(c);
+    }
+
+    @Test
+    public String find(){
+
+
+        return null;
     }
 }
